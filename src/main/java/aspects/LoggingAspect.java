253ed3dce642610ca50be96edb5e3628b1f99cf5
imamhsn195 <imamhsn195@gmail.com>
2024-12-27 00:47:22 +0400

    JoinPoint with arguments

--- src/main/java/aspects/LoggingAspect.java
+++ src/main/java/aspects/LoggingAspect.java
@@ -1,24 +1,24 @@
 package aspects;
 
+import models.Circle;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
-    @Before("allGetMethods() && allCircleMethods()")
-    public void loggingAllGetMethodsAdvice(){
-        System.out.println("All get methods advice.");
-    }
+   @Before("allMethodsCircle()")
+    public void adviceAllMethodCircle(JoinPoint joinPoint){
+//       Circle circle = (Circle) joinPoint.getTarget();
+//       System.out.println(joinPoint.getTarget());
+   }
 
-//    @Before("allCircleMethods()")
-//    public void loggingAllCircleMethods(){
-//        System.out.println("All Circle class Methods");
-//    }
+   @Before("args(name)")
+   public void stringArgumentMethods(String name){
+       System.out.println("Advice printed that takes String arguments " + name);
+   }
 
-    @Pointcut("execution(* *.get*())")
-    public void allGetMethods(){}
-
-    @Pointcut("within(models.Circle)")
-    public void allCircleMethods(){}
+   @Pointcut("within(models.Circle)")
+    public void allMethodsCircle(){}
 }
